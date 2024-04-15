7800f9824521c0e1380031d2f6870d083b04985c
Omar Gaxiola <66231336+omarigg04@users.noreply.github.com>
2024-04-15 01:01:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/herokuubsmi'));
+app.use(express.static('./dist/ml-dash'));
 
-app.get('/*', (req, res) =U+003E
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+app.get('/*', (req, res) 
+    res.sendFile('index.html', {root: 'dist/ml-dash/'}),
 );
 
 // Start the app by listening on the default Heroku port
