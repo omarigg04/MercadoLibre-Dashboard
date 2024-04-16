000af6008d9bdf7592b37c69b8687ec216624ab3
omarigg04 <gaxiolaomar04@gmail.com>
2024-04-15 18:55:45 -0600

    server fixes

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 //Install express server
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT;
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/ml-dash'));
 
-app.get('/*', (req, res) 
-    res.sendFile('index.html', {root: 'dist/ml-dash/'}),
+app.get('/*', function(req, res)
+    {res.sendFile('index.html', {root: 'dist/ml-dash/'})}
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+  });
